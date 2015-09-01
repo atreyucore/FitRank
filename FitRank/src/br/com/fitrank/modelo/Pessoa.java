f78aa810fed1643462df6e297c9d464210b28f0d
Gabriel <gabriel_vinicius92@hotmail.com>
2015-08-31 21:46:18 -0300

    incluindo coluna

--- FitRank/src/br/com/fitrank/modelo/Pessoa.java
+++ FitRank/src/br/com/fitrank/modelo/Pessoa.java
@@ -5,21 +5,29 @@ package br.com.fitrank.modelo;
  */
 public class Pessoa {
 //	Column(name="id_usuario", Primary Key)
-	private String idUsuario;
+	private String id_usuario;
 //	Column(name="nome")
 	private String nome;
+//	Column(name="data_cadastro")
+	private String data_cadastro;
 	
-	public String getIdUsuario() {
-		return idUsuario;
-	}
-	public void setIdUsuario(String idUsuario) {
-		this.idUsuario = idUsuario;
-	}
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(String id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+	public String getData_cadastro() {
+		return data_cadastro;
+	}
+	public void setData_cadastro(String data_cadastro) {
+		this.data_cadastro = data_cadastro;
 	}
 	
 }
