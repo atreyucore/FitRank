edcdb9020b2e94c2e59a34cf50e52e0558db538f
Gabriel <gabriel_vinicius92@hotmail.com>
2015-09-16 01:42:09 -0300

    Removendo constante de formatação de data. Este deve ficar no conversor de datas para que o sistema utilize somente o formato atual.

--- src/br/com/fitrank/util/ConstantesFitRank.java
+++ src/br/com/fitrank/util/ConstantesFitRank.java
@@ -3,7 +3,6 @@ package br.com.fitrank.util;
 
 public final class ConstantesFitRank {
 
-	public static final String FORMATO_DATA = "dd/MM/yyyy";
 	public static final String NOITE = "N";
 	public static final String DIA = "D";
 	public static final String MODALIDADE_CAMINHADA = "W";
