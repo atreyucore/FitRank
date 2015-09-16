4dc155c3087a3a85c6cfa25dd93ae94e99773dcc
Gabriel <gabriel_vinicius92@hotmail.com>
2015-09-16 01:39:55 -0300

    Restringindo o conversor para objetos Date.

---
+++ src/br/com/fitrank/util/DateConversor.java
@@ -0,0 +1,24 @@
+package br.com.fitrank.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateConversor {
+	
+	private static final String FORMATO_DATA = "dd/MM/yyyy";
+	private static final SimpleDateFormat FORMATTER = new SimpleDateFormat(FORMATO_DATA);
+	
+	public static String DateToString(Date data) {
+		return FORMATTER.format(data);
+	}
+	
+	public static Date StringToDate(String data){
+		try {
+			return FORMATTER.parse(data);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
