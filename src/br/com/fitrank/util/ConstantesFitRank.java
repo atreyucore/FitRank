9ae8ab909af0d02ad0c88247aa4a5d9e760b4fde
Gabriel <gabriel_vinicius92@hotmail.com>
2015-10-04 18:32:36 -0300

    Ajuste para receber Configuração ao invés de Pessoa para gerar ranking.

--- src/br/com/fitrank/util/ConstantesFitRank.java
+++ src/br/com/fitrank/util/ConstantesFitRank.java
@@ -5,6 +5,9 @@ public final class ConstantesFitRank {
 
 	public static final String NOITE = "N";
 	public static final String DIA = "D";
+	public static final String SEMANA = "S";
+	public static final String MES = "M";
+	public static final String ANO = "A";
 	public static final String MODALIDADE_CAMINHADA = "W";
 	public static final String MODALIDADE_BICICLETA = "B";
 	public static final String MODALIDADE_CORRIDA = "R";
