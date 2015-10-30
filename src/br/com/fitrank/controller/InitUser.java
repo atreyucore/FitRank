d294ef397ed535f4140babbd10b0062ece7adb4b
Daquer <leandro.moniz@gmail.com>
2015-10-29 22:41:00 -0200

    Resolvendo erro de merge do InitUser

--- src/br/com/fitrank/controller/InitUser.java
+++ src/br/com/fitrank/controller/InitUser.java
@@ -19,81 +19,62 @@ import com.restfb.Parameter;
 import com.restfb.types.User;
 
 public class InitUser extends HttpServlet {
-
+	
 	private static final long serialVersionUID = 1L;
-
+	
 	PessoaServico pessoaServico = new PessoaServico();
 	AmizadeServico amizadeServico = new AmizadeServico();
+   
+    public InitUser() {
+      
+    }
 
-	public InitUser() {
-
-	}
-
-	protected void inicia(HttpServletRequest request, HttpServletResponse response)
+   
+  protected void inicia(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
 	   
 	   FacebookClient facebookClient = new DefaultFacebookClient(request.getParameter("token"));
-
 	   User facebookUser = facebookClient.fetchObject("me", User.class);
-	   
 	   Connection<User> friendsFB = facebookClient.fetchConnection("me/friends", User.class, Parameter.with("fields", "name, id"));
+	   
 	   
 	   Pessoa pessoa = new Pessoa();
 	   
-	   if(facebookUser.getId()!=null && !facebookUser.getId().equals("")){
-		   pessoa.setId_usuario(facebookUser.getId());
-	   }
-		
-	   if(facebookUser.getFirstName()!=null){
-		   pessoa.setNome(facebookUser.getName());
+	   if (pessoaServico.lePessoaServico(facebookUser) == null ) {
+		   pessoa = pessoaServico.adicionaPessoaServico(facebookUser);
 	   }
 	   
-	   if (pessoaServico.lePessoaServico(facebookUser) == null ) {
-		   pessoa = pessoaServico.adicionaPessoaServico(pessoa);
-	   } else {
-		   pessoa = pessoaServico.atualizaPessoaServico(pessoa);
-	   }
-	   	
 //	   String[] profPicUrls = new String[friendsFB.getData().size()];
 //	   String[] ids = new String[friendsFB.getData().size()];
-//	   for (int i=0;i < friendsFB.getData().size(); i++) {
-	   for ( User friendFB : friendsFB.getData()) {
-//		  String idAmigo = friendsFB.getData().get(i).getId();
-		   
-		  atualizaAmizadeUsuario(facebookUser, friendFB);
-
+	   for (int i=0;i < friendsFB.getData().size(); i++) {
+		  String idAmigo = friendsFB.getData().get(i).getId();
+		  
+		  if (amizadeServico.leAmizadeServico(facebookUser.getId(), idAmigo) == null ) {
+			  amizadeServico.adicionaAmizadeServico(facebookUser.getId(), idAmigo);
+		  }
 //		  ids[i] = id;
 //		  profPicUrls[i] = friendsFB.getData().get(i).getPicture() != null ? friendsFB.getData().get(i).getPicture().getUrl() : "imagem/401.png" ;
 		 
 	   }
 	   
-	   
-		   
-	   
-//TODO gvsribeiro utilizar dados da configuracao do usuario para montar proxima tela 
+//TODO	   Recuperar configuração de favorito aqui!
 	   request.setAttribute("token", request.getParameter("token"));
+	   
+//	   request.setAttribute("ids", ids);
+//	   request.setAttribute("profPicUrl", profPicUrls);
+	   
 	   
 	   RequestDispatcher rd = request.getRequestDispatcher("/escolheModalidade.jsp");  
 	   rd.forward(request,response);  
    }
-
-	private void atualizaAmizadeUsuario(User facebookUser, User friendFB) {
-		
-		String idAmigo = friendFB.getId();
-		
-		if (amizadeServico.leAmizadeServico(facebookUser.getId(), idAmigo) == null) {
-			amizadeServico.adicionaAmizadeServico(facebookUser.getId(), idAmigo);
-		}
-	}
-
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		inicia(request, response);
-
+		
 	}
 
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		inicia(request, response);
 	}
 
