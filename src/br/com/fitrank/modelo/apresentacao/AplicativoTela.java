81c02ad3a106fda3ba2737c991d260de6dad82eb
Gabriel <gabriel_vinicius92@hotmail.com>
2016-04-13 22:34:23 -0300

    Ajustes em atualização, acrescentando logo de apps e ranking no período "sempre". AplicativoTela criado para apresentação de quantidade de atividades por app na tela do ranking.

---
+++ src/br/com/fitrank/modelo/apresentacao/AplicativoTela.java
@@ -0,0 +1,16 @@
+package br.com.fitrank.modelo.apresentacao;
+
+import br.com.fitrank.modelo.Aplicativo;
+
+public class AplicativoTela extends Aplicativo {
+	private Integer quantidadeAtividades;
+
+	public Integer getQuantidadeAtividades() {
+		return quantidadeAtividades;
+	}
+
+	public void setQuantidadeAtividades(Integer quantidadeAtividades) {
+		this.quantidadeAtividades = quantidadeAtividades;
+	}
+
+}
