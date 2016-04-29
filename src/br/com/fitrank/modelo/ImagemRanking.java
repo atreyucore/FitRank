3e460e69c7fbab81c0c0d680f0a1fb5e2f885840
Gabriel <gabriel_vinicius92@hotmail.com>
2016-04-28 21:39:26 -0300

    Classe para representar a tabela imagem_ranking no banco.

---
+++ src/br/com/fitrank/modelo/ImagemRanking.java
@@ -0,0 +1,26 @@
+package br.com.fitrank.modelo;
+
+import com.mysql.jdbc.Blob;
+
+public class ImagemRanking {
+	
+	private int id_ranking;
+	
+	private Blob imagem;
+
+	public int getId_ranking() {
+		return id_ranking;
+	}
+
+	public void setId_ranking(int id_ranking) {
+		this.id_ranking = id_ranking;
+	}
+
+	public Blob getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(Blob imagem) {
+		this.imagem = imagem;
+	}
+}
